949944b26142389d9df299002d38c6f516c79777
Christian Rapp <christian.rapp@edu.varberg.se>
2023-10-29 23:02:25 +0100

    Add project files.

---
+++ Labb 2 - Arv/Djur.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_2___Arv
+{
+    public class Djur
+    {
+        public string Namn { get; set; }
+        public int Ålder { get; set; }
+        public string Färg { get; set; }
+        public string ÖgonFärg { get; set; }
+        public double Vikt { get; set; }
+        public string Art { get; set; }
+        public string Egenskap { get; set; }
+
+        public Djur(string namn, int ålder, string färg, double vikt, string art, string egenskap)
+        {
+            Namn = namn;
+            Ålder = ålder;
+            Färg = färg;
+            Vikt = vikt;
+            Art = art;
+            Egenskap = egenskap;
+        }
+        public void Sträcka()
+        {
+            Console.WriteLine($"{Namn} Sträcker på sig långsamt");
+        }
+        public void Äta()
+        {
+
+            Console.WriteLine($"{Namn} äter.");
+        }
+
+        public void Sova()
+        {
+            Console.WriteLine($"{Namn} sover.");
+
+        }
+
+        public virtual void MakeSound()
+        {
+            Console.WriteLine($"{Namn} gör lite ljud av sig");
+        }
+
+        public void kissa()
+        {
+            Console.WriteLine($"{Namn} som är en {Art} behöver kissa");
+        }
+
+        public virtual void klia()
+        {
+            Console.WriteLine($"{Namn} kliar sig med foten");
+        }
+
+        public void gäspa()
+        {
+            Console.WriteLine($"{Namn} gäspar högt");
+        }
+
+
+        public void Svans()
+        {
+            Console.WriteLine($"{Namn} viftar på svansen");
+        }
+
+
+    }
+
+  
+
+    class Tax : Djur
+    {
+        public string Egenskap { get; set; }
+        public int AntalBen { get; set; }
+
+        public Tax(string namn, int ålder, string färg, double vikt, string ras, string egenskap, int antalBen)
+        : base(namn, ålder, färg, vikt, "Hund", egenskap)
+        {
+            Egenskap = egenskap;
+            AntalBen = 4;
+
+        }
+
+       
+
+        public void Skälla()
+        {
+            Console.WriteLine($"{Namn} skäller högt!");
+        }
+
+
+    }
+    class Franskbulldog : Djur
+    {
+        public string Egenskap { get; set; }
+        public bool KanSima { get; set; }
+
+        public Franskbulldog(string namn, int ålder, string färg, double vikt, string ras,string egenskap, bool kanSima)
+        : base(namn, ålder, färg, vikt, "Hund", egenskap)
+        {
+            Egenskap = egenskap;
+            KanSima = true;
+
+        }
+
+
+        public void Snarka()
+        {
+            Console.WriteLine($"{Namn} snarkar sött.");
+        }
+
+
+
+    }
+    
+    class Chihuahua : Djur
+    {
+
+        public string Egenskap { get; set; }
+        public string FavoritLeksak { get; set; }
+        public Chihuahua(string namn, int ålder, string färg, double vikt, string ras, string egenskap, string favoritleksak)
+       : base(namn, ålder, färg, vikt, "Hund", egenskap)
+        {
+
+            Egenskap = egenskap;
+            FavoritLeksak = FavoritLeksak;
+
+        }
+
+        public void Skutta()
+        {
+            Console.WriteLine($"{Namn} skuttar glatt runt.");
+        }
+
+
+    }
+
+
+    class StorTax : Tax
+    {
+        public string Egenskap { get; set; }
+        public double Snabb {  get; set; }
+
+        public StorTax(string namn, int ålder, string färg, double vikt, string ras, string egenskap, int antalBen, double snabb)
+          : base(namn, ålder, färg, vikt, ras, egenskap, antalBen)
+        {
+            Egenskap = egenskap;
+            Snabb = snabb;
+        }
+
+        public void JagaByten()
+        {
+            Console.WriteLine($"{Namn} jagar byten med stor entusiasm.");
+        }
+    }
+
+
+    class LitenTax : Tax
+    {
+        public string Egenskap { get; set; }
+        public int Djup {  get; set; }
+
+        public LitenTax(string namn, int ålder, string färg, double vikt, string ras, string egenskap, int antalBen, int djup)
+          : base(namn, ålder, färg, vikt, ras, egenskap, antalBen)
+        {
+            Egenskap = egenskap;
+            Djup = djup;
+        }
+
+        public void Grävahål()
+        {
+            Console.WriteLine($"{Namn} Gräver ett hål med sina små tassar");
+        }
+
+
+    }
+
+
+}
---
+++ Labb 2 - Arv/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Win32.SafeHandles;
+
+namespace Labb_2___Arv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tax tax = new Tax("Ubbe", 3, "Svart", 5.6, "Dvärg-Tax", "kan se ut som en lakrits", 4);
+            Franskbulldog Fransk = new Franskbulldog("Berit", 7, "beige", 11, "Franskbulldog", "kan simma baklänges", false);
+            Chihuahua Chi = new Chihuahua("Einar", 5, "Vit", 3, "Chihuahua", "gillar att sola", "bollar");
+            StorTax Nippe = new StorTax("Nippe", 11, "Brun", 6.1, "Strävhårig Tax", "Gillar att rymma", 4, 20.0);
+            LitenTax Helge = new LitenTax("Helge", 9, "Grå", 5.9, "Dvärg Tax", "Korta ben", 4, 2);
+
+
+            Console.WriteLine($"{tax.Namn} är en {tax.Art}, hans päls är {tax.Färg}, han är också {tax.Ålder} gammal och väger {tax.Vikt} kg, {tax.Egenskap}");
+            tax.MakeSound();
+            tax.Sträcka();
+            tax.Skälla();
+            
+            Console.WriteLine(" ");
+
+
+            Console.WriteLine($"{Fransk.Namn} är en {Fransk.Art}, hennes päls är {Fransk.Färg}, hon är också {Fransk.Ålder} gammal och väger {Fransk.Vikt} kg, {Fransk.Egenskap}");
+            Fransk.MakeSound();
+            Fransk.Snarka();
+            
+            Console.WriteLine(" ");
+
+
+            Console.WriteLine($"{Chi.Namn} är en {Chi.Art}, hans päls är {Chi.Färg}, han är också {Chi.Ålder} gammal och väger {Chi.Vikt} kg och kan {Chi.Egenskap}");
+            Chi.MakeSound();
+            Chi.Äta();
+            Chi.Skutta();
+            
+            Console.WriteLine(" ");
+
+
+            Console.WriteLine($"{Nippe.Namn} är en {Nippe.Art}, hans päls är {Nippe.Färg}, han är också {Nippe.Ålder} gammal och väger {Nippe.Vikt} kg och kan {Nippe.Egenskap}");
+            Nippe.MakeSound();
+            Nippe.JagaByten();
+
+            Console.WriteLine(" ");
+
+           
+            Console.WriteLine($"{Helge.Namn} är en {Helge.Art}, hans päls är {Helge.Färg}, han är också {Helge.Ålder} gammal och väger {Helge.Vikt} kg och kan {Helge.Egenskap}");
+            Helge.MakeSound();
+            Helge.Grävahål();
+            
+            Console.WriteLine(" ");
+            
+        }
+    }
+}
